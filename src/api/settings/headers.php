b0dd5a0917f06147d79ebea85ba6753d2529c5cd
CARLOS GABRIEL MAGALLANES LOPEZ <a.24308051280865@cbtis128.edu.mx>
2026-06-09 01:12:50 -0600

    Remove unnecessary PHP opening tag from headers.php

--- src/api/settings/headers.php
+++ src/api/settings/headers.php
@@ -1,9 +1,5 @@
 <?php
-
-// Encabezados para uso compartido de recursos entre orígenes (CORS) y tipo de contenido
 header("Access-Control-Allow-Origin: *");                                              // Permitir solicitudes de cualquier origen
 header("Content-Type: application/json; charset=UTF-8");                               // Establecer el tipo de contenido y codificación
 header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT");                        // Permitir métodos HTTP específicos
 header("Access-Control-Allow-Headers: Content-Type");                                  // Permitir encabezados HTTP específicos
-
-?>  
