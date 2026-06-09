b7da95a15e950f5ecc98bf790e2d62a3a7ffc8b7
CARLOS GABRIEL MAGALLANES LOPEZ <a.24308051280865@cbtis128.edu.mx>
2026-06-09 01:10:22 -0600

    Fix file path for autoload in index.php

--- src/api/index.php
+++ src/api/index.php
@@ -4,7 +4,7 @@
 declare(strict_types = 1);
 
 // Dpendencias Internas y Externas
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 require_once  __DIR__ . '/settings/headers.php';    
 require_once __DIR__ . '/controllers/router.php';
 require_once __DIR__ . '/settings/middleware.php';
